1b48d63f1a9214a7d6f069b7ed36bec21cd8ed0a
Mauro Adamo <101474625+MRAS23@users.noreply.github.com>
2022-08-11 12:39:24 -0300

    Agregar boton jugar (#4)
    * agrega boton
    
    se va a usar este boton para iniciar el juego
    
    * cambia clase
    
    se quiere que las cartas no sean visibles hasta que se inicie el juego
    
    * Update index.html
    
    * agrega funcion de inicialJuego y mostrarTablero
    
    se quiere que al clickear el boton jugar el juego inicie, primero va a asignar los colores aleatoreamente a las cartas y luego va a motrar el tablero con todas las cartas
    
    * agrega funciones de bloquear/desbloquear input jugador
    
    se quiere que el input se bloquee cuando el jugador ya haya seleccionado dos cartas, una vez mostradas y fijadas si acertó o ocultada en caso de que no se vuelve a activar el input para que el jugador pueda volver a seleccionar otro par de cartas.

--- main.js
+++ main.js
@@ -2,6 +2,13 @@ const $cartas = document.querySelectorAll(".carta");
 
 let colores = ["red", "blue", "green", "yellow", "purple", "orange"];
 
+document.querySelector("#boton-jugar").onclick = iniciarJuego;
+
+function iniciarJuego() {
+  asignarColorAleatorioCartas();
+  mostrarTablero();
+}
+
 function obtenerNumeroAleatorio() {
   return Math.round(Math.random() * 11);
 }
@@ -18,8 +25,6 @@ function asignarColorAleatorioCartas() {
   }
 }
 
-asignarColorAleatorioCartas();
-
 let cartasSeleccionadas = [];
 
 function manejarInputJugador(e) {
@@ -32,12 +37,14 @@ function manejarInputJugador(e) {
   }
 
   if (cartasSeleccionadas.length === 2) {
+    bloquearInputJugador();
     if (verificarCoincidencias()) {
       eliminarColorAcertado();
       cartasSeleccionadas = [];
     }
     setTimeout(() => {
       ocultarCartas();
+      desbloquearInputJugador();
     }, 1000);
 
     console.log(verificarCoincidencias());
@@ -77,4 +84,26 @@ function eliminarColorAcertado() {
       colores.splice(i, 1);
     }
   }
+}
+
+function mostrarTablero() {
+  const $tablero = document.querySelector("#tablero");
+  $tablero.className = "container";
+}
+
+function ocultarTablero() {
+  const $tablero = document.querySelector("#tablero");
+  $tablero.className = "oculto";
+}
+
+function bloquearInputJugador() {
+  $cartas.forEach(function ($carta) {
+    $carta.onclick = function () {};
+  });
+}
+
+function desbloquearInputJugador() {
+  $cartas.forEach(function ($carta) {
+    $carta.onclick = manejarInputJugador;
+  });
 }
